b6c8cacb67182604b3ad991b4e4ff336001e8365
techwulf <t.zach.moore@gmail.com>
2021-09-28 15:59:46 -0600

    implement get /api/posts/:id fetch a post

--- api/posts/posts-router.js
+++ api/posts/posts-router.js
@@ -8,10 +8,28 @@ router.get('/', async (req, res) => {
   try {
     const posts = await Post.find();
     res.status(200).json(posts);
-  } catch (err) {
+  } catch {
     res.status(500).json({
       "message":"The posts information could not be retrieved"
     });
+  }
+});
+
+router.get('/:id', async (req, res) => {
+  try {
+    const {id} = req.params;
+    const post = await Post.findById(id);
+    if (post) {
+      res.status(200).json(post);
+    } else {
+      res.status(404).json({
+        "message":"The post with the specified ID does not exist"
+      });
+    }
+  } catch {
+    res.status(500).json({
+      "message":"The post information could not be retrieved"
+    })
   }
 });
 
