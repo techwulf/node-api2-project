9264541ccfef8a72f1ec32f0574dcde7153b1810
techwulf <t.zach.moore@gmail.com>
2021-09-28 17:13:18 -0600

    implement post /api/posts add a new post

--- api/posts/posts-router.js
+++ api/posts/posts-router.js
@@ -29,7 +29,27 @@ router.get('/:id', async (req, res) => {
   } catch {
     res.status(500).json({
       "message":"The post information could not be retrieved"
-    })
+    });
+  }
+});
+
+router.post('/', async (req, res) => {
+  try {
+    const {title, contents} = req.body;
+    if (!title || !contents) {
+      res.status(400).json({
+        "message":"Please provide title and contents for the post"
+      });
+    } else {
+      const {id} = await Post.insert({title, contents});
+      const post = await Post.findById(id);
+      console.log(post);
+      res.status(201).json(post);
+    }
+  } catch {
+    res.status(500).json({
+      "message":"There was an error while saving the post to the database"
+    });
   }
 });
 
