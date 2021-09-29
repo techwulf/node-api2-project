eeb5a9d9f9f7f9a3993cd9104c68f5fb2859d834
techwulf <t.zach.moore@gmail.com>
2021-09-28 18:25:47 -0600

    implement put /api/posts/:id edit an existing post

--- api/posts/posts-router.js
+++ api/posts/posts-router.js
@@ -43,13 +43,39 @@ router.post('/', async (req, res) => {
     } else {
       const {id} = await Post.insert({title, contents});
       const post = await Post.findById(id);
-      console.log(post);
       res.status(201).json(post);
     }
   } catch {
     res.status(500).json({
       "message":"There was an error while saving the post to the database"
     });
+  }
+});
+
+router.put('/:id', async (req, res) => {
+  try {
+    const {id} = req.params;
+    const postToEdit = await Post.findById(id);
+    if (postToEdit) {
+      const post = req.body;
+      if (!post.title || !post.contents) {
+        res.status(400).json({
+          "message":"Please provide title and contents for the post"
+        });
+      } else {
+        const editedPost = await Post.update(id, post);
+        console.log(editedPost);
+        res.status(200).json({id: editedPost, ...post});
+      }
+    } else {
+      res.status(404).json({
+        "message":"The post with the specified ID does not exist"
+      });
+    }
+  } catch {
+    res.status(500).json({
+      "message":"The post information could not be modified"
+    })
   }
 });
 
