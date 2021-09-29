888e0f118f8daa6302fb0d81c8cf8c322988c58a
techwulf <t.zach.moore@gmail.com>
2021-09-28 19:11:15 -0600

    implement get /api/posts/:id/comments fetch post comments

--- api/posts/posts-router.js
+++ api/posts/posts-router.js
@@ -93,8 +93,27 @@ router.delete('/:id', async (req, res) => {
   } catch {
     res.status(500).json({
       "message":"The comments information could not be retrieved"
-    })
+    });
   }
-})
+});
+
+router.get('/:id/comments', async (req, res) => {
+  try {
+    const {id} = req.params;
+    const post = await Post.findById(id);
+    if (post) {
+      const comments = await Post.findPostComments(id);
+      res.status(200).json(comments);
+    } else {
+      res.status(404).json({
+        "message":"The post with the specified ID does not exist"
+      });
+    }
+  } catch {
+    res.status(500).json({
+      "message":"The comments information could not be retrieved"
+    });
+  }
+});
 
 module.exports = router;
